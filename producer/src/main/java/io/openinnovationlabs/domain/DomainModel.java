2a79ca6c5ed0fa32bb5d3982706c75f49d347cf9
Justin Holmes <jholmes@redhat.com>
2018-07-09 08:17:12 -0600

    comment

--- producer/src/main/java/io/openinnovationlabs/domain/DomainModel.java
+++ producer/src/main/java/io/openinnovationlabs/domain/DomainModel.java
@@ -31,6 +31,9 @@ public class DomainModel {
                 if (ar.cause() instanceof ReplyException) {
                     if (((ReplyException) ar.cause()).failureType().equals(ReplyFailure.NO_HANDLERS)) {
                         LOGGER.info(ar.cause().getLocalizedMessage());
+
+                        // what happens if more than one command is issued before the verticle comes up?
+                        // dead letter here ?
                         deployAggregrateVerticle(command);
                     }
                 } else {
@@ -42,6 +45,7 @@ public class DomainModel {
 
     /**
      * TODO handle rehydration from event log
+     * use Akka as the example https://doc.akka.io/docs/akka/current/persistence.html
      */
     private void deployAggregrateVerticle(Command command) {
         JsonObject config = new JsonObject().put("id", command.aggregateId);
