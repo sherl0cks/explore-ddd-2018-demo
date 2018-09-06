6c1d118cb3ae9c0e614a0ce9ca6dc070192bdec7
Justin Holmes <jholmes@redhat.com>
2018-09-05 19:19:55 -0600

    update start script

--- bpm/kie-server/etc/start_kie-server.sh
+++ bpm/kie-server/etc/start_kie-server.sh
@@ -18,7 +18,7 @@ echo "KIE_MAVEN_REPO: $KIE_MAVEN_REPO"
 JBOSS_ARGUMENTS=" -b $JBOSS_BIND_ADDRESS -Dorg.kie.server.id=$KIE_SERVER_ID -Dorg.kie.server.user=$KIE_SERVER_USER -Dorg.kie.server.pwd=$KIE_SERVER_PWD -Dorg.kie.server.location=$KIE_SERVER_LOCATION "
 
 # Controller argument for the KIE Execution server, as we assume controller exists
-JBOSS_ARGUMENTS="$JBOSS_ARGUMENTS -Dorg.kie.server.controller=$KIE_SERVER_CONTROLLER -Dorg.kie.server.controller.user=$KIE_SERVER_CONTROLLER_USER -Dorg.kie.server.controller.pwd=$KIE_SERVER_CONTROLLER_PWD "
+JBOSS_ARGUMENTS="$JBOSS_ARGUMENTS -Dorg.kie.server.controller=$KIE_SERVER_CONTROLLER -Dorg.kie.server.controller.user=$KIE_SERVER_CONTROLLER_USER -Dorg.kie.server.controller.pwd=$KIE_SERVER_CONTROLLER_PWD -Dkie.maven.settings.custom=$JBOSS_HOME/../.m2/settings.xml"
 
 # Start Wildfly with the given arguments.
 echo "Running KIE Execution Server on JBoss Wildfly..."
