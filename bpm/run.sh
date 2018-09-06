94c672fc7fe4444e62e23a6e32b41ad4e6b30045
Justin Holmes <jholmes@redhat.com>
2018-09-06 12:26:16 -0600

    update run s2i

--- bpm/run.sh
+++ bpm/run.sh
@@ -16,8 +16,8 @@ oc volume dc/kie-workbench --add -m /opt/jboss/wildfly/git --claim-size=1G --nam
 
 oc new-build --name kie-server-base https://github.com/sherl0cks/explore-ddd-2018-demo --context-dir bpm/kie-server
 oc new-build --name kie-server --binary=true --strategy=source -i kie-server-base
-oc set triggers bc/kie-server --auto
-oc new-app --name kie-server -i kie-server \
+
+oc new-app kie-server \
     -e KIE_SERVER_CONTROLLER=http://kie-workbench:8080/jbpm-console/rest/controller \
     -e KIE_MAVEN_REPO=http://kie-workbench:8080/jbpm-console/maven2 \
     -e JAVA_OPTS='-server -Xms256m -Xmx1024m -Djava.net.preferIPv4Stack=true -Dkie.maven.settings.custom=$JBOSS_HOME/../.m2/settings.xml' \
